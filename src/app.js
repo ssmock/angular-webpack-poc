7a0a238e5efe08a18cb008eb8390f4096d019742
steve <ssmock@gmail.com>
2015-02-03 09:48:08 -0500

    initial

---
+++ src/app.js
@@ -0,0 +1,26 @@
+﻿require("angular"); // Global
+require("angular-route");
+
+require("./views/a.html");
+require("./views/b.html");
+
+angular.module("app", ["ngRoute"]).config(config);
+
+config.$inject = ["$routeProvider"];
+
+function config($routeProvider) {
+    $routeProvider
+        .when("/main", {
+            templateUrl: "main.html",
+            controller: "Main",
+            controllerAs: "Main"
+        })
+        .when("/a", {
+            templateUrl: "a.html"
+        })
+        .when("/b", {
+            templateUrl: "b.html"
+        });
+}
+
+require("./main-controller");
---
+++ src/main-controller.js
@@ -0,0 +1,15 @@
+﻿require("angular");
+require("./some-service");
+
+require('./views/main.html');
+
+angular.module("app").controller("Main", mainController);
+
+mainController.$inject = [];
+
+function mainController() {
+    var vm = this;
+
+    vm.TestVal = "Test value!";
+    vm.TestAr = [1, 2, 3, 4, 5];
+}
